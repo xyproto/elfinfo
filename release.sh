8012f9218c515cac1009a869d03eec24dc7eef0b
Alexander F. Rødseth <rodseth@gmail.com>
2019-09-16 12:12:57 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ver=$(git describe --tags)
 mkdir -p "elfinfo-$ver"
-cp -rv main.go LICENSE README.md vendor go.mod go.sum testfiles "elfinfo-$ver"
+cp -rv main.go LICENSE README.md vendor go.mod go.sum "elfinfo-$ver"
 tar Jcvf "elfinfo-$ver.tar.xz" "elfinfo-$ver"
