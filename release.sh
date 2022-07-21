c4a00ee84845a39c0b19b819400f6cb56eae98e8
Alexander F. Rødseth <rodseth@gmail.com>
2022-07-21 13:18:27 +0200

    Update the release script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-ver=$(git describe --tags)
+ver=$(git describe --tags | cut -dv -f2)
+echo "Version: $ver"
 mkdir -p "elfinfo-$ver"
 cp -rv main.go LICENSE README.md vendor go.mod go.sum "elfinfo-$ver"
 tar Jcvf "elfinfo-$ver.tar.xz" "elfinfo-$ver"
